34d6092c47f6d2e199322aa6b7afb44122f4bf2c
hibana2077 <hibana2077@gmail.com>
2024-04-07 23:50:38 +0800

    Update README.md with improved introduction to paper writing lab project

---
+++ lab/paper_writing/main.py
@@ -0,0 +1,22 @@
+'''
+Author: hibana2077 hibana2077@gmail.com
+Date: 2024-04-07 23:40:44
+LastEditors: hibana2077 hibana2077@gmail.com
+LastEditTime: 2024-04-07 23:49:46
+FilePath: \llm_agent_flow_lab\lab\paper_writing\main.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AEch
+'''
+from tqdm import tqdm
+from langchain_groq import ChatGroq
+import logging
+import time
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()

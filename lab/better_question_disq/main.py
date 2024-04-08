a5b43a1614f84745852858dc8d4afd00d4bd64d2
hibana2077 <hibana2077@gmail.com>
2024-04-08 08:32:15 +0800

    Update main.py with workflow comments and remove unused imports

--- lab/better_question_disq/main.py
+++ lab/better_question_disq/main.py
@@ -2,15 +2,13 @@
 Author: hibana2077 hibana2077@gmail.com
 Date: 2024-04-07 23:39:59
 LastEditors: hibana2077 hibana2077@gmail.com
-LastEditTime: 2024-04-08 08:27:15
+LastEditTime: 2024-04-08 08:32:08
 FilePath: \llm_agent_flow_lab\lab\better_question_disq\main.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
+# Work flow: user input -> answer generation -> self answer check -> answer output  
 from tqdm import tqdm
 from langchain_groq import ChatGroq
-from langchain_community.document_loaders import ArxivLoader, PyMuPDFLoader, _module_lookup
-# more document loaders see _module_lookup in langchain_community.document_loaders
-from langchain_core.documents import Document
 import logging
 import time
 
